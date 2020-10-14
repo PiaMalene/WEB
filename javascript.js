af6cc0d8962aa2990f53a9a66249fcf2faa30dfa
Haris Brkic <55392120+HarisBrkic@users.noreply.github.com>
2020-10-14 12:47:48 +0200

    Test

---
+++ javascript.js
@@ -0,0 +1,14 @@
+/* Kode skrevet av Aleksander, sist endret 07.09.2020 */
+
+function openMenu() { //Funksjonen åpner menyen på mobil når hamburgeren blir trykket på
+    var x = document.getElementById("linker");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function changeMenu(x) { //Funksjonen endrer utseende av hamburgeren etter den er trykket på
+    x.classList.toggle("change");
+  } 
