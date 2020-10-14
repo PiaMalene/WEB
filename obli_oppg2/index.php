5eddf6af3fd60e0db6c63bd13ff6516b8ea678a2
PiaMalene <piaros@hotmail.no>
2020-10-14 15:59:38 +0200

    Update index.php

--- obli_oppg2/index.php
+++ obli_oppg2/index.php
@@ -1,5 +1,15 @@
-<?php 
-include("styling/header.php");
-
-
-?>
+<!DOCTYPE html>
+<html lang="no">
+<head>
+	<title>Hjem</title>
+	<?php include("styling/header.php");?>
+<h1>HCI og brukervennlig design</h1>
+<h3>oppsummerer hva emnet handler om, og hva dere har gjort.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</body>
+</html>
