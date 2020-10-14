27d313958dd37697388e92c3022ae42c1598c34d
PiaMalene <72134344+PiaMalene@users.noreply.github.com>
2020-10-14 12:49:36 +0200

    Add files via upload

---
+++ obli_oppg2/nyttige_lenker.php
@@ -0,0 +1,43 @@
+
+
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    
+    <title>Nyttige lenker</title>
+    <?php include("styling/header.php"); ?>
+
+
+    <h1>Forskjellige nyttige kilder i IT fag</h1>
+    <br>
+    <div>
+        <h3> html & CSS </h3>
+        <ul>
+        <li> <a href="https://www.w3schools.com/">w3schools |</a> HTML & CSS, JavaScript, Programming, Server side & XLM.</li>    
+        <li> <a href="https://stackoverflow.com/">stackoverflow |</a> <i class="fab fa-stack-overflow" style="color:orange;"></i> Diskusjonsforum for hjelp</li>
+        <li> <a href="https://www.w3schools.com/icons/fontawesome_icons_intro.asp"> Font awesome (forklaring) |</a> <i class="fab fa-font-awesome-alt fa-lg" style="color: #3B85DE;"></i> <a href="https://fontawesome.com/icons?d=gallery&m=free"> | Font awesome Ikon galleri  </a></li> 
+        <li> <a href="https://css-tricks.com/">! css-tricks</a>| Oppslagsverk for styling</li>
+        <li> <a href="https://www.w3schools.com/css/css_float_examples.asp">w3s: CSS Layout |</a> Float - Clear - boxes</li>
+        <li> <a href="https://htmlcolorcodes.com/">HtmlColorCodes |</a> Finne, velge & bruke farger i koding <br> <a href="http://www.colorsontheweb.com/Color-Tools/Color-Wizard">Colorsontheweb</a>| KomplimentærFarger</li>   
+        <li> <a href="https://memberservices.informit.com/my_account/webedition/9780135167724/html/index.html?link=0">Html oppslagsverk |</a> <i class="fas fa-dollar-sign" style="color: #31A412;"></i> NB! betalt produkt</li>
+    </ul>
+        <h3> Universell utforming</h3>
+        <ul>
+            <li> <a href="https://uu.difi.no/krav-og-regelverk/wcag-20-standarden">Difi.no |</a> WCAG: Krav og regelverk</li>
+            <li> <a href="http://checkers.eiii.eu/">! Page Checker</a>| Ser etter feil på en web-side</li>
+            <li> <a href="https://www.funka.com/no/universell-utforming/informasjon-web-og-ikt/anbefalinger/tips-for-universell-utforming/">Funka/universell-utforming</a>| uu-tips</li>
+            <li> <a href="https://webaim.org/resources/">Web-aim |</a> Accessibility Tester og kilder</li>
+            <li> <a href="https://www.interaction-design.org/literature/article/how-to-create-an-intuitive-design">Interaction Design</a>| How to Create an Intuitive Design</li>
+            <li> <a href="e-bok/ux-basics.pdf">Basics in UX-design </a>| PDF: E-bok om UX design</li>
+            <li> <a href="https://alistapart.com/blog/topic/design/">Alistapart</a>| Design; fargevalg, kultur, variasjon, ...</li>
+            
+        </ul>
+        <h3>Wireframes</h3>
+        <ul>
+            <li> <a href="http://framebox.org/">Framebox</a> | Lag et enkelt web-opsett</li>
+            <li> <a href="https://wireframe.cc/">Wireframe.cc</a> | Lag et enkelt web-opsett</li>
+            <li> <a href="https://jumpchart.com/">! Jumpchart</a>| planlegge en hjemmeside og linker til undersider</li>
+        </ul>
+    </div>
+</body>
+</html>
